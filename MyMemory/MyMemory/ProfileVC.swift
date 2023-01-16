d249a379bc0f88144d8b0140276c2164061d6f75
sudonnoh <sudonnoh@gmail.com>
2023-01-16 19:02:49 +0900

    2023.01.16

--- MyMemory/MyMemory/ProfileVC.swift
+++ MyMemory/MyMemory/ProfileVC.swift
@@ -43,12 +43,25 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         )
         self.navigationItem.leftBarButtonItem = backBtn
         
-        // 2. 이미지 설정
+        // 3. 셀 배경 이미지 설정
+        let bg = UIImage(named: "profile-bg")
+        let bgImg = UIImageView(image: bg)
+        
+        bgImg.frame.size = CGSize(width: bgImg.frame.size.width, height: bgImg.frame.size.height)
+        bgImg.center = CGPoint(x: self.view.frame.width / 2, y: 40)
+        
+        bgImg.layer.cornerRadius = bgImg.frame.size.width / 2
+        bgImg.layer.borderWidth = 0
+        bgImg.layer.masksToBounds = true
+        
+        self.view.addSubview(bgImg)
+        
+        // 2. 프로필 이미지 설정
         let image = UIImage(named: "account.jpg")
         
         self.profileImage.image = image
         self.profileImage.frame.size = CGSize(width: 100, height: 100)
-        self.profileImage.center = CGPoint(x: self.view.frame.width / 2, y: 160)
+        self.profileImage.center = CGPoint(x: self.view.frame.width / 2, y: 270)
         
         // 이미지 둥글게 만들기
         self.profileImage.layer.cornerRadius = self.profileImage.frame.width / 2
@@ -70,6 +83,13 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         self.tv.delegate = self
         
         self.view.addSubview(self.tv)
+        
+        /* 뷰 계층을 맞추려면 아래와 같이 입력 */
+        // self.view.bringSubviewToFront(self.tv)
+        // self.view.bringSubviewToFront(self.profileImage)
+        
+        // 내비게이션 바 숨김처리
+        self.navigationController?.navigationBar.isHidden = true
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -77,7 +97,22 @@ class ProfileVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "cell")
+        
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 14)
+        cell.detailTextLabel?.font = UIFont.systemFont(ofSize: 13)
+        cell.accessoryType = .disclosureIndicator
+        
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "이름"
+            cell.detailTextLabel?.text = "AXCE"
+        case 1:
+            cell.textLabel?.text = "계정"
+            cell.detailTextLabel?.text = "axce@naver.com"
+        default:
+            ()
+        }
         return cell
     }
     
