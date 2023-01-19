e2f05f7d48758ca749d12299f983bc6afb5f2361
sudonnoh <sudonnoh@gmail.com>
2023-01-19 19:44:17 +0900

    2023.01.19

--- Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
+++ Chapter06-SQLite3/Chapter06-SQLite3/ViewController.swift
@@ -12,15 +12,58 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // SQLite 연결 정보를 담을 객체
-        var db: OpaquePointer? = nil
-        // 컴파일된 SQL을 담을 객체
-        var stmt: OpaquePointer? = nil
-        
+        let dbPath = self.getDBPath()
+        self.dbExecute(dbPath: dbPath)
+    }
+    
+    func getDBPath() -> String {
         // 앱 내 문서 디렉토리 경로에서 SQLite DB 파일을 찾는다.
         let fileMgr = FileManager()
         let docPathURL = fileMgr.urls(for: .documentDirectory, in: .userDomainMask).first!
-        let dbPath = docPathURL.appendingPathComponent("db.sqlite").path
+        let dbPath = docPathURL.appending(path: "db.sqlite").path()
+        print("dbPath : \(dbPath)")
+        
+        // dbPath 경로에 db.sqlite 파일이 없다면 앱 번들에 만들어둔 db.sqlite 파일을 가져와 복사한다.
+        if fileMgr.fileExists(atPath: dbPath) == false {
+            let dbSource = Bundle.main.path(forResource: "db", ofType: "sqlite")
+            try! fileMgr.copyItem(atPath: dbSource!, toPath: dbPath)
+        }
+        return dbPath
+    }
+    
+    func dbExecute(dbPath: String) {
+        // SQLite 연결 정보를 담을 객체
+        var db: OpaquePointer? = nil
+        
+        guard sqlite3_open(dbPath, &db) == SQLITE_OK else {
+            print("Database Connect Fail")
+            return
+        }
+        
+        // 데이터베이스 연결 종료
+        defer {
+            print("Close Database Connection")
+            sqlite3_close(db)
+        }
+        
+        // 컴파일된 SQL을 담을 객체
+        var stmt: OpaquePointer? = nil
+        let sql = "CREATE TABLE IF NOT EXISTS sequence (num INTEGER)"
+        
+        guard sqlite3_prepare(db, sql, -1, &stmt, nil) == SQLITE_OK else {
+            print("Prepare Statement Fail")
+            return
+        }
+        
+        // stmt 변수 해제
+        defer {
+            print("Finalize Statement")
+            sqlite3_finalize(stmt)
+        }
+        
+        if sqlite3_step(stmt) == SQLITE_DONE {
+            print("Create Table Success!")
+        }
     }
 }
 
